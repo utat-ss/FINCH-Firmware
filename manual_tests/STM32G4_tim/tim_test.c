16a2e85a0ef5697f336597c0886f605029d17479
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2020-07-22 01:20:20 -0400

    FIRM16-4: added manual test folder and file template

---
+++ manual_tests/STM32G4_tim/tim_test.c
@@ -0,0 +1,10 @@
+/*
+ * tim_test.c
+ *
+ *  Created on: Jul 22, 2020
+ *      Author: Cameron Rodriguez
+ *
+ * Overview: TIM testing functions
+ */
+
+#include "../../src/drivers/STM32G4_tim/tim.h"
