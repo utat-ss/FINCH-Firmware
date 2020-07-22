be49788a3e4ee7780c7326e992d506b0a9c98ec0
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2020-07-22 00:39:53 -0400

    FIRM16-4: Created TIM files and fixed project include locations

---
+++ src/drivers/STM32G4_tim/tim.c
@@ -0,0 +1,10 @@
+/*
+ * tim.c
+ *
+ *  Created on: Jul 22, 2020
+ *      Author: Cameron Rodriguez
+ *
+ * Overview: Wrapper code for the HAL code for timer functions on the STM32G4 MCU
+ */
+
+#include "tim.h"
---
+++ src/drivers/STM32G4_tim/tim.h
@@ -0,0 +1,13 @@
+/*
+ * tim.h
+ *
+ *  Created on: Jul 22, 2020
+ *      Author: camro
+ */
+
+#ifndef DRIVERS_STM32G4_TIM_TIM_H_
+#define DRIVERS_STM32G4_TIM_TIM_H_
+
+
+
+#endif /* DRIVERS_STM32G4_TIM_TIM_H_ */
