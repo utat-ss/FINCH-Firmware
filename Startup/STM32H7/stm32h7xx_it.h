496638860380e04743cbc024b1126d83ddaf0793
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2020-08-04 05:50:34 -0400

    FIRM16-5: added initialization function

--- Startup/STM32H7/stm32h7xx_it.h
+++ Startup/STM32H7/stm32h7xx_it.h
@@ -26,8 +26,18 @@
  extern "C" {
 #endif
 
+/* Includes ------------------------------------------------------------------*/
+#include "../../Generated_Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h"
+#include "../../Generated_Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_ll_pwr.h"
+#include "../../Generated_Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+// General Libraries
+#include <stdint.h>
+// Driver Wrapper Includes
+#include "../../src/drivers/STM32G4_gpio/gpio.h"
+#include "../../src/drivers/STM32G4_tim/tim.h"
 
 /* USER CODE END Includes */
 
