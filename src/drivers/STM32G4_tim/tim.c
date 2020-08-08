965d65b2f684d24213b0892d17f0a67a4931e581
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2020-08-08 07:51:36 -0400

    FIRM16-5: Added manual TIM test

--- src/drivers/STM32G4_tim/tim.c
+++ src/drivers/STM32G4_tim/tim.c
@@ -12,7 +12,7 @@
 /* Populate the handle for a timer
  * @param TIM_HandleTypeDef handle - the handle to be populated
  * @param TIM_TypeDef* instance - the register for the timer
- * @param uint32_t prescaler - prescaler value to divide clock by
+ * @param uint32_t prescaler - prescaler value
  * @param uint32_t time_counter_mode - counting mode (TIM_COUNTERMODE[_UP/_DOWN/_CENTERALIGNED1/_CENTERALIGNED2/_CENTERALIGNED3])
  * @param uint32_t period - value to reset to (counting down) or to reset at (counting up)
  * @param uint32_t clock_divison - value to divide clock by (TIM_CLOCKDIVISION[_DIV1/_DIV2/_DIV4])
@@ -40,30 +40,49 @@ TIM_HandleTypeDef* tim_populate_handle(TIM_HandleTypeDef* handle, TIM_TypeDef* i
  * @param TIM_Encoder_InitTypeDef* sconfig - Encoder interface config handle
  * @return uint8_t - successful/failed initialization
  */
-uint8_t tim_init_function(TIM_HandleTypeDef* handle, uint16_t function, uint32_t onepulse_mode, TIM_Encoder_InitTypeDef* sConfig){
+uint8_t tim_init(TIM_HandleTypeDef* handle, uint16_t function, uint32_t onepulse_mode, TIM_Encoder_InitTypeDef* sConfig){
 	if(function==TIM_Base){
-		HAL_TIM_Base_MspInit(handle);
 		HAL_TIM_Base_Init(handle);
 	}else if(function==TIM_IC){
-		HAL_TIM_IC_MspInit(handle);
 		HAL_TIM_IC_Init(handle);
 	}else if(function==TIM_OC){
-		HAL_TIM_OC_MspInit(handle);
 		HAL_TIM_OC_Init(handle);
 	}else if(function==TIM_PWM){
-		HAL_TIM_PWM_MspInit(handle);
 		HAL_TIM_PWM_Init(handle);
 	}else if(function==TIM_OnePulse){
-		HAL_TIM_OnePulse_MspInit(handle);
 		HAL_TIM_OnePulse_Init(handle, onepulse_mode);
 	}else if(function==TIM_Encoder){
-		HAL_TIM_Encoder_MspInit(handle);
 		HAL_TIM_Encoder_Init(handle, sConfig);
 	}else{
 		return 0;
 	}
 
 	return 1;
+}
+
+/* DEinitialize a specific timer function
+ * @param TIM_HandleTypeDef* handle - the handle for the timer
+ * @param uint16_t function - the timer function to be disabled (see tim.h for potential values)
+ * @return uint8_t - successful/failed deinitialization
+ */
+uint8_t tim_deinit(TIM_HandleTypeDef* handle, uint16_t function){
+	if(function==TIM_Base){
+			HAL_TIM_Base_DeInit(handle);
+		}else if(function==TIM_IC){
+			HAL_TIM_IC_DeInit(handle);
+		}else if(function==TIM_OC){
+			HAL_TIM_OC_DeInit(handle);
+		}else if(function==TIM_PWM){
+			HAL_TIM_PWM_DeInit(handle);
+		}else if(function==TIM_OnePulse){
+			HAL_TIM_OnePulse_DeInit(handle);
+		}else if(function==TIM_Encoder){
+			HAL_TIM_Encoder_DeInit(handle);
+		}else{
+			return 0;
+		}
+
+		return 1;
 }
 
 /* Starts a timer peripheral, without DMA or interrupts
--- src/drivers/STM32G4_tim/tim.h
+++ src/drivers/STM32G4_tim/tim.h
@@ -15,7 +15,9 @@ enum tim_func_constants {TIM_Base, TIM_IC, TIM_OC, TIM_PWM, TIM_OnePulse, TIM_En
 
 TIM_HandleTypeDef* tim_populate_handle(TIM_HandleTypeDef* handle, TIM_TypeDef* instance, uint32_t prescaler, uint32_t time_counter_mode, uint32_t period,
 		uint32_t clock_division, uint32_t repitition_counter, uint32_t autoreload_preload);
-uint8_t tim_init_function(TIM_HandleTypeDef* handle, uint16_t function, uint32_t onepulse_mode, TIM_Encoder_InitTypeDef* sConfig);
+uint8_t tim_init(TIM_HandleTypeDef* handle, uint16_t function, uint32_t onepulse_mode, TIM_Encoder_InitTypeDef* sConfig);
+uint8_t tim_deinit(TIM_HandleTypeDef* handle, uint16_t function);
 uint8_t tim_start(TIM_HandleTypeDef* handle, uint16_t function, uint32_t channel);
 uint8_t tim_stop(TIM_HandleTypeDef* handle, uint16_t function, uint32_t channel);
+
 #endif /* DRIVERS_STM32G4_TIM_TIM_H_ */
