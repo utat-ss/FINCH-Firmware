cba1c311955e3ad38cac6e39c3ffd1cfcc6dbc35
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2020-08-20 22:49:13 -0400

    FIRM16-5: Added DMA start/stop and timer struct with timer structs and variables

--- src/drivers/STM32_tim/tim.h
+++ src/drivers/STM32_tim/tim.h
@@ -10,6 +10,20 @@
 
 #include "../../Core/Inc/main.h"
 
+// Struct for timer handle and associated values, and template struct
+typedef struct {
+	TIM_HandleTypeDef *handle;
+	TIM_IC_InitTypeDef *ic_cfg;
+	TIM_OC_InitTypeDef *oc_cfg;
+	TIM_OnePulse_InitTypeDef *onepulse_cfg;
+	TIM_Encoder_InitTypeDef *encoder_cfg;
+	TIM_ClockConfigTypeDef *clk_cfg;
+	TIM_SlaveConfigTypeDef *slave_cfg;
+	uint16_t function;
+	uint32_t channel;
+	uint32_t onepulse_mode;
+} TimFunc;
+
 // Constants for timer functions
 enum tim_func_constants {TIM_Base, TIM_IC, TIM_OC, TIM_PWM, TIM_OnePulse, TIM_Encoder};
 
@@ -24,14 +38,18 @@ void tim_disable_IT_flag(TIM_HandleTypeDef *handle, uint32_t flag);
 uint8_t tim_get_IT_flag(TIM_HandleTypeDef *handle, uint32_t flag);
 uint8_t tim_clear_IT_flag(TIM_HandleTypeDef *handle, uint32_t flag);
 
-// Timer init/start/stop functions
+// Timer config functions
 TIM_HandleTypeDef *tim_populate_handle(TIM_HandleTypeDef *handle, TIM_TypeDef *instance, uint32_t prescaler, uint32_t time_counter_mode, uint32_t period,
 		uint32_t clock_division, uint32_t repitition_counter, uint32_t autoreload_preload);
-uint8_t tim_init(TIM_HandleTypeDef *handle, uint16_t function, uint32_t onepulse_mode, TIM_Encoder_InitTypeDef* sConfig);
-uint8_t tim_deinit(TIM_HandleTypeDef *handle, uint16_t function);
-uint8_t tim_start(TIM_HandleTypeDef *handle, uint16_t function, uint32_t channel);
-uint8_t tim_start_IT(TIM_HandleTypeDef *handle, uint16_t function, uint32_t channel);
-uint8_t tim_stop(TIM_HandleTypeDef *handle, uint16_t function, uint32_t channel);
-uint8_t tim_stop_IT(TIM_HandleTypeDef *handle, uint16_t function, uint32_t channel);
+
+// Timer init/start/stop functions
+uint8_t tim_init(TIM_HandleTypeDef *handle, uint8_t function, uint32_t onepulse_mode, TIM_Encoder_InitTypeDef *encoder_cfg);
+uint8_t tim_deinit(TIM_HandleTypeDef *handle, uint8_t function);
+uint8_t tim_start(TIM_HandleTypeDef *handle, uint8_t function, uint32_t channel);
+uint8_t tim_start_IT(TIM_HandleTypeDef *handle, uint8_t function, uint32_t channel);
+uint8_t tim_start_DMA(TIM_HandleTypeDef *handle, uint8_t function, uint32_t channel, uint32_t *src_addr, uint32_t *src_addr2, uint16_t length);
+uint8_t tim_stop(TIM_HandleTypeDef *handle, uint8_t function, uint32_t channel);
+uint8_t tim_stop_IT(TIM_HandleTypeDef *handle, uint8_t function, uint32_t channel);
+uint8_t tim_stop_DMA(TIM_HandleTypeDef *handle, uint8_t function, uint32_t channel);
 
 #endif /* DRIVERS_STM32_TIM_TIM_H_ */
