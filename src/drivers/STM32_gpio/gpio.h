fed6e8e47af38d236f64784de3da8a59019d6625
Brytni Richards <brytniri@gmail.com>
2020-08-27 23:35:48 -0400

    FIRM16-15: added alternate functions to GPIO

--- src/drivers/STM32_gpio/gpio.h
+++ src/drivers/STM32_gpio/gpio.h
@@ -25,9 +25,18 @@ typedef struct {
 	uint16_t pin;
 } GPIO_OUTPUT;
 
+typedef struct {
+	GPIO_TypeDef *port;
+	uint16_t mode;
+	uint16_t pin;
+	uint8_t alternate;
+} GPIO_ALT;
+
 /* Functions */
 GPIO_OUTPUT gpio_init_output(GPIO_TypeDef *port, uint16_t pin);
 GPIO_OUTPUT gpio_init_output_od(GPIO_TypeDef *port, uint16_t pin, uint32_t pull);
+GPIO_ALT gpio_init_alt(GPIO_TypeDef *port, uint16_t pin, uint8_t alt);
+GPIO_ALT gpio_init_alt_od(GPIO_TypeDef *port, uint16_t pin, uint32_t pull, uint8_t alt);
 GPIO_INPUT gpio_init_input(GPIO_TypeDef *port, uint16_t pin);
 void gpio_set_high(GPIO_OUTPUT gpio);
 void gpio_set_low(GPIO_OUTPUT gpio);
