bd2a5bc9bd32f54e81eb57bf434247e75899da17
Tobias Rozario <tobi.rozario@gmail.com>
2021-01-30 22:53:56 -0500

    created sdram constructor function. now need to set up reading and writing into memory

--- Src/drivers/STM32_fmc_sdram/fmc_sdram.c
+++ Src/drivers/STM32_fmc_sdram/fmc_sdram.c
@@ -8,28 +8,39 @@
 
 #include "fmc_sdram.h"
 
-void fmc_sdram_init() {
+static uint32_t FMC_Initialized = 0;
+
+void fmc_sdram_init(FMC_SDRAM* sdmram_handler, uint32_t bank_num, uint32_t mem_data_width) {
+  sdram_handler->bank_num = bank_num;
+  sdram_handler->mem_data_width = mem_data_width;
+  sdram_handler->bank_num = bank_num;
+  sdram_handler->num_row_bits = 13;
+  sdram_handler->num_col_bits = 8;
+
+
+  if (bank_num == FMC_SDRAM_BANK1){
+    sdram_handler->base_address = 0xC0000000;
+  }
+  else if (bank_num == FMC_SDRAM_BANK2) {
+    sdram_handler->base_address = 0xD0000000;
+  }
 
   FMC_SDRAM_TimingTypeDef SdramTiming = {0};
 
-  /* USER CODE BEGIN FMC_Init 1 */
-
-  /* USER CODE END FMC_Init 1 */
-
   /** Perform the SDRAM1 memory initialization sequence
   */
-  hsdram1.Instance = FMC_SDRAM_DEVICE;
-  /* hsdram1.Init */
-  hsdram1.Init.SDBank = FMC_SDRAM_BANK1;
-  hsdram1.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_8;
-  hsdram1.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_11;
-  hsdram1.Init.MemoryDataWidth = FMC_SDRAM_MEM_BUS_WIDTH_8;
-  hsdram1.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
-  hsdram1.Init.CASLatency = FMC_SDRAM_CAS_LATENCY_1;
-  hsdram1.Init.WriteProtection = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
-  hsdram1.Init.SDClockPeriod = FMC_SDRAM_CLOCK_DISABLE;
-  hsdram1.Init.ReadBurst = FMC_SDRAM_RBURST_DISABLE;
-  hsdram1.Init.ReadPipeDelay = FMC_SDRAM_RPIPE_DELAY_0;
+  sdram_handler->hsdram.Instance = FMC_SDRAM_DEVICE;
+  /* sdram_handler -> hsdram.Init */
+  sdram_handler->hsdram.Init.SDBank = bank_num - 1;
+  sdram_handler->hsdram.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_8;
+  sdram_handler->hsdram.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_13;
+  sdram_handler->hsdram.Init.MemoryDataWidth = mem_data_width;
+  sdram_handler->hsdram.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
+  sdram_handler->hsdram.Init.CASLatency = FMC_SDRAM_CAS_LATENCY_1;
+  sdram_handler->hsdram.Init.WriteProtection = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
+  sdram_handler->hsdram.Init.SDClockPeriod = FMC_SDRAM_CLOCK_DISABLE;
+  sdram_handler->hsdram.Init.ReadBurst = FMC_SDRAM_RBURST_DISABLE;
+  sdram_handler->hsdram.Init.ReadPipeDelay = FMC_SDRAM_RPIPE_DELAY_0;
   /* SdramTiming */
   SdramTiming.LoadToActiveDelay = 16;
   SdramTiming.ExitSelfRefreshDelay = 16;
@@ -39,41 +50,97 @@ void fmc_sdram_init() {
   SdramTiming.RPDelay = 16;
   SdramTiming.RCDDelay = 16;
 
-  if (HAL_SDRAM_Init(&hsdram1, &SdramTiming) != HAL_OK)
+  //Initialize GPIO Settings
+  GPIO_InitTypeDef GPIO_InitStruct ={0};
+    if (FMC_Initialized) {
+      return;
+    }
+    FMC_Initialized = 1;
+
+    /* Peripheral clock enable */
+    __HAL_RCC_FMC_CLK_ENABLE();
+
+    /** FMC GPIO Configuration
+    PF0   ------> FMC_A0
+    PF1   ------> FMC_A1
+    PF2   ------> FMC_A2
+    PF3   ------> FMC_A3
+    PF4   ------> FMC_A4
+    PF5   ------> FMC_A5
+    PC0   ------> FMC_SDNWE
+    PC2_C   ------> FMC_SDNE0
+    PC3_C   ------> FMC_SDCKE0
+    PF11   ------> FMC_SDNRAS
+    PF12   ------> FMC_A6
+    PF13   ------> FMC_A7
+    PF14   ------> FMC_A8
+    PF15   ------> FMC_A9
+    PG0   ------> FMC_A10
+    PG1   ------> FMC_A11
+    PE7   ------> FMC_D4
+    PE8   ------> FMC_D5
+    PE9   ------> FMC_D6
+    PE10   ------> FMC_D7
+    PD14   ------> FMC_D0
+    PD15   ------> FMC_D1
+    PG2   ------> FMC_A12
+    PG4   ------> FMC_BA0
+    PG5   ------> FMC_BA1
+    PG8   ------> FMC_SDCLK
+    PD0   ------> FMC_D2
+    PD1   ------> FMC_D3
+    PG15   ------> FMC_SDNCAS
+    PB5   ------> FMC_SDCKE1
+    PB6   ------> FMC_SDNE1
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                            |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_11|GPIO_PIN_12
+                            |GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_2|GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_4
+                            |GPIO_PIN_5|GPIO_PIN_8|GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+    HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  if (HAL_SDRAM_Init(&sdram_handler -> hsdram, &SdramTiming) != HAL_OK)
   {
     Error_Handler( );
   }
-
-  /** Perform the SDRAM2 memory initialization sequence
-  */
-  hsdram2.Instance = FMC_SDRAM_DEVICE;
-  /* hsdram2.Init */
-  hsdram2.Init.SDBank = FMC_SDRAM_BANK2;
-  hsdram2.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_8;
-  hsdram2.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_11;
-  hsdram2.Init.MemoryDataWidth = FMC_SDRAM_MEM_BUS_WIDTH_8;
-  hsdram2.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
-  hsdram2.Init.CASLatency = FMC_SDRAM_CAS_LATENCY_1;
-  hsdram2.Init.WriteProtection = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
-  hsdram2.Init.SDClockPeriod = FMC_SDRAM_CLOCK_DISABLE;
-  hsdram2.Init.ReadBurst = FMC_SDRAM_RBURST_DISABLE;
-  hsdram2.Init.ReadPipeDelay = FMC_SDRAM_RPIPE_DELAY_0;
-  /* SdramTiming */
-  SdramTiming.LoadToActiveDelay = 16;
-  SdramTiming.ExitSelfRefreshDelay = 16;
-  SdramTiming.SelfRefreshTime = 16;
-  SdramTiming.RowCycleDelay = 16;
-  SdramTiming.WriteRecoveryTime = 16;
-  SdramTiming.RPDelay = 16;
-  SdramTiming.RCDDelay = 16;
-
-  if (HAL_SDRAM_Init(&hsdram2, &SdramTiming) != HAL_OK)
-  {
-    Error_Handler( );
-  }
-
-  /* USER CODE BEGIN FMC_Init 2 */
-
-  /* USER CODE END FMC_Init 2 */
 
 }
--- Src/drivers/STM32_fmc_sdram/fmc_sdram.h
+++ Src/drivers/STM32_fmc_sdram/fmc_sdram.h
@@ -8,7 +8,29 @@
 #ifndef DRIVERS_STM32_FMC_SDRAM_FMC_SDRAM_H_
 #define DRIVERS_STM32_FMC_SDRAM_FMC_SDRAM_H_
 
-void fmc_sdram_init();
+#ifdef STM32H743xx
+#include <stm32h7xx_hal.h>
+#include <stm32h7_startup.h>
+#include <drivers/STM32H7_clock/clock.h>
+#endif
+
+#ifdef STM32G474xx
+#include <stm32g4xx_hal.h>
+#include <stm32g4_startup.h>
+#include <drivers/STM32G4_clock/clock.h>
+#endif
+
+typedef struct {
+  SDRAM_HandleTypeDef hsdram;
+  uint32_t bank_num;
+  uint32_t mem_data_width;
+  uint32_t num_row_bits;
+  uint32_t num_col_bits;
+  uint32_t base_address;
+
+}FMC_SDRAM;
+
+void fmc_sdram_init(FMC_SDRAM* sdmram_handler, uint32_t bank_num, uint32_t bank_num, uint32_t mem_data_width);
 
 
 #endif /* DRIVERS_STM32_FMC_SDRAM_FMC_SDRAM_H_ */
