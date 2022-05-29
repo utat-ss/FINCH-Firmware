3e690a2ec37008f0ac15cd869218de1df967967f
Eric Leus <ericleus@outlook.com>
2022-05-28 23:47:42 -0400

    FIRM34: Added G431 src folder and G431RBConfig.h

---
+++ Src/nucleo_g431rb/G431RBConfig.h
@@ -0,0 +1,32 @@
+/*
+Date Created: May 28, 2022
+Last Modified: May 28, 2022
+
+I/O and settings configuration information for STM32G431RB dev kit 
+
+Author: EricLeus
+*/
+
+#ifndef NUCLEO_G431RB_G431RBCONFIG_H_
+#define NUCLEO_G431RB_G431RBCONFIG_H_
+
+// General
+#define G431RB_BOARD    MCU_BOARD_NUCLEO_G431RB
+
+// UART
+#define G431RB_UART_INST    LPUART1
+#define G431RB_UART_TX_PORT GPIOA
+#define G431RB_UART_TX_PIN  GPIO_PIN_2
+#define G431RB_UART_RX_PORT GPIOA
+#define G431RB_UART_RX_PIN  GPIO_PIN_3
+#define G431RB_UART_AF      GPIO_AF12_LPUART1
+
+// User button
+#define G431RB_BLUE_BUTTON_PORT GPIOC
+#define G431RB_BLUE_BUTTON_PIN  GPIO_PIN_13
+
+// Green LED
+#define G431RB_GREEN_LED_PORT   GPIOA
+#define G431RB_GREEN_LED_PIN    GPIO_PIN_5
+
+#endif /* NUCLEO_G431RB_G431RBCONFIG_H_ */
